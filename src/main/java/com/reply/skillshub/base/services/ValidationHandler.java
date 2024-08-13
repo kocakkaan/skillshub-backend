6df2c96c43844c67d05f403d8a878039e09c85b9
maurits <maurits.de.roover@outlook.com>
2024-08-13 07:07:11 +0200

    add validationHandler and a configuration to load the default Validator to avoid having to load entire SpringBootContext

---
+++ src/main/java/com/reply/skillshub/base/services/ValidationHandler.java
@@ -0,0 +1,45 @@
+package com.reply.skillshub.base.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.reply.skillshub.base.exceptionhandling.exeptions.ObjectDetail;
+import com.reply.skillshub.base.exceptionhandling.exeptions.ValidationException;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ValidationHandler<T> {
+
+    private Logger LOG = LoggerFactory.getLogger(ValidationHandler.class);
+
+    private final Validator validator;
+
+    public void validate(T entity) {
+        Set<ConstraintViolation<T>> violations = validator.validate(entity);
+        if (!violations.isEmpty()) {
+            
+            List<ObjectDetail> details = new ArrayList<>();
+
+            violations.forEach(violation -> {
+                ObjectDetail objectDetail = new ObjectDetail();
+                objectDetail.setTopic(violation.getPropertyPath().toString());
+                objectDetail.setDetail(violation.getMessage());
+                details.add(objectDetail);
+            });
+            
+            LOG.error("validation failed", violations);
+            
+            throw new ValidationException(details);
+        }
+    }
+    
+}
---
+++ src/test/java/com/reply/skillshub/data/company/TestConfiguration.java
@@ -0,0 +1,20 @@
+package com.reply.skillshub.data.company;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    Validator validator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        return factory.getValidator();
+    }
+
+    
+}
