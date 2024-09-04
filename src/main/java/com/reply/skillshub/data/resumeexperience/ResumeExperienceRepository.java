5d0fe5c2eab4dbfac8d53e4be02690a18eb9805c
maurits <maurits.de.roover@outlook.com>
2024-09-04 16:22:03 +0200

    add resume experience repository

---
+++ src/main/java/com/reply/skillshub/data/resumeexperience/ResumeExperienceRepository.java
@@ -0,0 +1,8 @@
+package com.reply.skillshub.data.resumeexperience;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+public interface ResumeExperienceRepository extends Neo4jRepository<ResumeExperience, String> {
+
+    
+}
