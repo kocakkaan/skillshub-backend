3c5a5c8a0593620263b8eecff46314cc967d6b52
Maurits De Roover <maurits.de.roover@outlook.com>
2025-02-20 16:55:08 +0100

    implement the export again since it dissappeared somehow

--- src/main/java/com/reply/skillshub/controllers/resume/ResumeController.java
+++ src/main/java/com/reply/skillshub/controllers/resume/ResumeController.java
@@ -4,7 +4,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +32,7 @@ public class ResumeController implements ResumesApi {
   private final PowerPointService powerPointService;
 
   @Override
-  public ResponseEntity<Void> exportToPptx(String resumeId, String language, String company) {
+  public ResponseEntity<Resource> exportToPptx(String resumeId, String language, String company) {
     var resume = resumeControllerService.findResumeEntityById(resumeId);
     var pptDto = powerPointService.createPowerPointDto(resume, language, company);
     var ppt = powerPointService.createPowerPointFromTemplate(pptDto);
@@ -40,10 +43,18 @@ public class ResumeController implements ResumesApi {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    // powerPointService.createPowerPoint();
-    // TODO Auto-generated method stub
-    // ResponseEntity.
-    return ResponseEntity.status(200).build();
+
+    Resource resource = new ByteArrayResource(test.toByteArray());
+
+         // Set the response headers
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.presentationml.presentation"));
+    headers.setContentDispositionFormData("attachment", "presentation.pptx");
+
+    // Return the presentation as a response entity
+    return ResponseEntity.ok()
+            .headers(headers)
+            .body(resource);
   }
 
   @Override
