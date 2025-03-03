5eb6598c08eaefca1abaf48a1ee6982a5709e3c6
Maurits De Roover <maurits.de.roover@outlook.com>
2025-03-03 10:15:37 +0100

    update SkillsAgentService to deal with errors coming from the service

--- src/main/java/com/reply/skillshub/services/SkillsAgentService.java
+++ src/main/java/com/reply/skillshub/services/SkillsAgentService.java
@@ -1,5 +1,6 @@
 package com.reply.skillshub.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
@@ -12,20 +13,21 @@ public class SkillsAgentService {
 
     private final WebClient webClient;
 
-    @Value("${skillhub.agent}") 
-    private String agent;
-
-    public SkillsAgentService(WebClient.Builder webcBuilder) {
+    public SkillsAgentService(WebClient.Builder webcBuilder, @Value("${skillhub.agent}") String agent) {
         this.webClient = webcBuilder.baseUrl(agent).build();
     }
 
     public List<String> getKeywordsFromSearchString(String queryString) {
-        List<String> returnValue = webClient
+        try {
+            List<String> returnValue = webClient
             .get()
             .uri((uriBuilder) -> uriBuilder.path("/keywords").queryParam("text", queryString).build())
             .retrieve()
             .bodyToMono(new ParameterizedTypeReference<List<String>>() {})
             .block();
-        return returnValue;
+            return returnValue;
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 }
