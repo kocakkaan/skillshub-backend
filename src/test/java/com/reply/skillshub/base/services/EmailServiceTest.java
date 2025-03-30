8b713a2cec3f7b4fdb5779d1f8e56d66eb7eb913
Maurits De Roover <maurits.de.roover@outlook.com>
2025-03-30 16:29:07 +0200

    exclude email tests from general testing

--- src/test/java/com/reply/skillshub/base/services/EmailServiceTest.java
+++ src/test/java/com/reply/skillshub/base/services/EmailServiceTest.java
@@ -1,5 +1,6 @@
 package com.reply.skillshub.base.services;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,7 @@ class EmailServiceTest {
     private EmailService emailService;
 
     @Test
+    @Ignore
     void testEmailSend() {
         var emailRequest = new EmailRequest();
         var context = new Context();
