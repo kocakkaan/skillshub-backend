1cbed378125702749dc586f6baac4cd2e35e3d8a
Maurits De Roover <maurits.de.roover@outlook.com>
2025-04-07 08:55:25 +0200

    improvements to short cv generation

---
+++ src/test/java/com/reply/skillshub/resume/ShortCvGeneratorUtilsTest.java
@@ -0,0 +1,65 @@
+package com.reply.skillshub.resume;
+
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+
+import com.reply.skillshub.controllers.resume.ShortCvGeneratorUtils;
+import com.reply.skillshub.data.resume.ShortCv;
+import com.reply.skillshub.data.resumeexperience.ResumeExperience;
+import com.reply.skillshub.services.GeneratedShortCv;
+import com.reply.skillshub.services.GeneratedShortCv.CvExperience;
+
+public class ShortCvGeneratorUtilsTest {
+
+  @Test
+  void testConvertToShortCv() {
+    GeneratedShortCv generatedShortCv = Instancio.of(GeneratedShortCv.class)
+        .supply(field(GeneratedShortCv.CvExperience::getDescriptions),
+            () -> Instancio.ofList(GeneratedShortCv.CvExperience.class).size(5).create())
+        .create();
+
+    ShortCv shortCv = ShortCvGeneratorUtils.convertToShortCv(generatedShortCv);
+
+    assertEquals(generatedShortCv.getProfessionalBackground(), shortCv.getBackground());
+    assertEquals(generatedShortCv.getIndustries(), shortCv.getIndustries());
+    assertEquals(generatedShortCv.getSkills().size(), shortCv.getSkills().size());
+    assertEquals(generatedShortCv.getExperiences().size(), shortCv.getExperiences().size());
+
+    for (int i = 0; i < generatedShortCv.getExperiences().size(); i++) {
+      CvExperience generatedExperience = generatedShortCv.getExperiences().get(i);
+      ResumeExperience shortCvExperience = shortCv.getExperiences().get(i);
+      assertEquals(generatedExperience.getTitle(), shortCvExperience.getTitle());
+      assertEquals(generatedExperience.getRole(), shortCvExperience.getRole());
+      assertEquals(generatedExperience.getDescriptions().size(), shortCvExperience.getDescriptions().size());
+      assertEquals(generatedExperience.getDescriptions(), shortCvExperience.getDescriptions());
+    }
+  }
+
+  @Test
+  void testConvertToShortCvMoreAsSixExperienceDescriptions() {
+    GeneratedShortCv generatedShortCv = Instancio.of(GeneratedShortCv.class)
+        .supply(field(GeneratedShortCv.CvExperience::getDescriptions),
+            () -> Instancio.ofList(GeneratedShortCv.CvExperience.class).size(7).create())
+        .create();
+
+    ShortCv shortCv = ShortCvGeneratorUtils.convertToShortCv(generatedShortCv);
+
+    assertEquals(generatedShortCv.getProfessionalBackground(), shortCv.getBackground());
+    assertEquals(generatedShortCv.getIndustries(), shortCv.getIndustries());
+    assertEquals(generatedShortCv.getSkills().size(), shortCv.getSkills().size());
+    assertEquals(generatedShortCv.getExperiences().size(), shortCv.getExperiences().size());
+
+    for (int i = 0; i < generatedShortCv.getExperiences().size(); i++) {
+      CvExperience generatedExperience = generatedShortCv.getExperiences().get(i);
+      ResumeExperience shortCvExperience = shortCv.getExperiences().get(i);
+      assertEquals(generatedExperience.getTitle(), shortCvExperience.getTitle());
+      assertEquals(generatedExperience.getRole(), shortCvExperience.getRole());
+      assertEquals(6, shortCvExperience.getDescriptions().size());
+      assertEquals(generatedExperience.getDescriptions().subList(0, 6), shortCvExperience.getDescriptions());
+    }
+  }
+
+}
