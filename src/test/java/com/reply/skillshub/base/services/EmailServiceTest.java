53be0065c644fa800d0a4358ccbf81a33d1a9d7c
Maurits De Roover <maurits.de.roover@outlook.com>
2025-05-19 07:54:53 +0200

    no more emails!

--- src/test/java/com/reply/skillshub/base/services/EmailServiceTest.java
+++ src/test/java/com/reply/skillshub/base/services/EmailServiceTest.java
@@ -1,47 +1,42 @@
 package com.reply.skillshub.base.services;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.thymeleaf.context.Context;
-
-import com.reply.skillshub.data.EmailRequest;
 
 @Import(ThymeleafTestConfig.class)
 @SpringBootTest(classes = { EmailService.class })
 class EmailServiceTest {
 
-    @Autowired
-    private EmailService emailService;
+    // @Autowired
+    // private EmailService emailService;
 
-    @Test
-    void testEmailSend() {
-        var emailRequest = new EmailRequest();
-        var context = new Context();
-        context.setVariable("title", "Is this a title?");
-        context.setVariable("link", "https://chat.mistral.ai");
-        context.setVariable("username", "Maurits De Roover");
-        emailRequest.setTemplate("confirmation");
-        emailRequest.setRecipient("maurits.de.roover@outlook.com");
-        emailRequest.setSubject("Test");
-        emailRequest.setContext(context);
-        emailService.sendEmail(emailRequest);
-    }
+    // @Test
+    // void testEmailSend() {
+    //     var emailRequest = new EmailRequest();
+    //     var context = new Context();
+    //     context.setVariable("title", "Is this a title?");
+    //     context.setVariable("link", "https://chat.mistral.ai");
+    //     context.setVariable("username", "Maurits De Roover");
+    //     emailRequest.setTemplate("confirmation");
+    //     emailRequest.setRecipient("maurits.de.roover@outlook.com");
+    //     emailRequest.setSubject("Test");
+    //     emailRequest.setContext(context);
+    //     emailService.sendEmail(emailRequest);
+    // }
 
     
-    @Test
-    void testNewUserSend() {
-        var emailRequest = new EmailRequest();
-        var context = new Context();
-        context.setVariable("title", "Is this a title?");
-        context.setVariable("link", "https://chat.mistral.ai");
-        context.setVariable("username", "Maurits De Roover");
-        emailRequest.setTemplate("new-user");
-        emailRequest.setRecipient("maurits.de.roover@outlook.com");
-        emailRequest.setSubject("Test");
-        emailRequest.setContext(context);
-        emailService.sendEmail(emailRequest);
-    }
+    // @Test
+    // void testNewUserSend() {
+    //     var emailRequest = new EmailRequest();
+    //     var context = new Context();
+    //     context.setVariable("title", "Is this a title?");
+    //     context.setVariable("link", "https://chat.mistral.ai");
+    //     context.setVariable("username", "Maurits De Roover");
+    //     emailRequest.setTemplate("new-user");
+    //     emailRequest.setRecipient("maurits.de.roover@outlook.com");
+    //     emailRequest.setSubject("Test");
+    //     emailRequest.setContext(context);
+    //     emailService.sendEmail(emailRequest);
+    // }
 
 }
