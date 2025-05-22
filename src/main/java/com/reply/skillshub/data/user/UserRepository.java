299a33a8cdeb157fd7e90d775b5f973ce3af71cb
Arnold Unterauer <a.unterauer@reply.de>
2025-05-22 09:14:20 +0200

    add query to find employees by company IDs

--- src/main/java/com/reply/skillshub/data/user/UserRepository.java
+++ src/main/java/com/reply/skillshub/data/user/UserRepository.java
@@ -24,6 +24,11 @@ public interface UserRepository extends Neo4jRepository<User, String> {
 
     List<User> findBySpeaksLanguageLanguageCode(LanguageCode languageCode);
 
+    @Query("""
+            MATCH (u:User)-[r:WORKS_FOR]->(c:Company)
+            WHERE c.id IN $company
+            RETURN u
+            """)
     List<Employee> findByCompaniesIdIn(List<String> company);
 
     <T> T findByResumesId(String resumeId, Class<T> type);
@@ -37,18 +42,19 @@ public interface UserRepository extends Neo4jRepository<User, String> {
             where size(skills) > 0 or size(certs) > 0
             return u
             """)
-    List<Employee> findByCompaniesIdAndSkillsLabelInOrHasCertificatesCertificateNameIn(String companyId, List<String> skills, List<String> certificates);
-
+    List<Employee> findByCompaniesIdAndSkillsLabelInOrHasCertificatesCertificateNameIn(String companyId,
+            List<String> skills, List<String> certificates);
 
     @Query("""
-        match (u:User) - [r:WORKS_FOR] -> (c:Company where c.id in $companyId)
-        optional match (u) - [h_s:HAS_SKILL] -> (skill:Skill where toLower(skill.label) in $skills)
-        with u, collect(skill) as skills
-        optional match (u) - [h_c:HAS_CERTIFICATE] -> (cert:Certificate where toLower(cert.name) in $certificates)
-        with u, skills, collect(cert) as certs
-        where size(skills) > 0 or size(certs) > 0
-        return u
-        """)
-    List<Employee> findByCompaniesIdInAndSkillsLabelInOrHasCertificatesCertificateNameIn(List<String> companyId, List<String> skills, List<String> certificates);
+            match (u:User) - [r:WORKS_FOR] -> (c:Company where c.id in $companyId)
+            optional match (u) - [h_s:HAS_SKILL] -> (skill:Skill where toLower(skill.label) in $skills)
+            with u, collect(skill) as skills
+            optional match (u) - [h_c:HAS_CERTIFICATE] -> (cert:Certificate where toLower(cert.name) in $certificates)
+            with u, skills, collect(cert) as certs
+            where size(skills) > 0 or size(certs) > 0
+            return u
+            """)
+    List<Employee> findByCompaniesIdInAndSkillsLabelInOrHasCertificatesCertificateNameIn(List<String> companyId,
+            List<String> skills, List<String> certificates);
 
 }
